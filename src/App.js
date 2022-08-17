dd0e03da2b3f5e4e69e96608fc386b644174c791
akram-webdev <akram.webdev@gmail.com>
2022-08-17 15:04:57 +0600

    all route setap done and it works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Blogs from './Components/Blogs/Blogs';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
+import Navbar from './Pages/Shared/Navbar';
+import NotFound from './Pages/Shared/NotFound';
 
 function App() {
   return (
     <div >
-      <Blogs/>
+       <Navbar/>
+       <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/Home' element={<Home/>}></Route>
+          <Route path='/blogs' element={<Blogs/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
+
+
+          <Route path='*' element={<NotFound/>}></Route>
+
+       </Routes>
     </div>
   );
 }
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            
+            <h1>This is Blogs
+            </h1>
         </div>
     );
 };
