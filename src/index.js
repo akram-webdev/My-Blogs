e5fda4bdfc013b6dc3a30f36b22214e6ca0f5d2c
akram-webdev <akram.webdev@gmail.com>
2022-08-17 17:29:48 +0600

    material tailwind installation doen and navbar done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,11 +6,14 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ThemeProvider>
       <App />
+    </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
